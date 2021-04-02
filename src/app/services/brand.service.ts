c25be9323b4c030b902c8f41dfb6bedf989daca9
MehmetUgur7 <77232030+MehmetUgur7@users.noreply.github.com>
2021-04-02 11:42:55 +0300

    2 nisan 1 commit

--- src/app/services/brand.service.ts
+++ src/app/services/brand.service.ts
@@ -1,20 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Car } from '../models/car';
+import { Brand } from '../models/brand';
 import { ListResponseModel } from '../models/ListResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CarService {
+export class BrandService {
 
-  apiUrl= "https://localhost:44348/api/cars/getall";
+  apiUrl= "https://localhost:44348/api/brands/getall";
 
   constructor(private httpClient:HttpClient) { }
 
-  getCars():Observable<ListResponseModel<Car>>{
-    return this.httpClient.get<ListResponseModel<Car>>(this.apiUrl)    
+  getBrands():Observable<ListResponseModel<Brand>>{
+    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl)    
   }
 
 }
